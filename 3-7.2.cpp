d8835146500452915d97e8f4f7c3211f288c7f34
wentengo <1910387599@qq.com>
2019-03-15 20:09:26 +0800

    进制转换

---
+++ 3-7.2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	string s, table = "0123456789ABCDEF";
+	int m, n;
+	cin >> m >> n;
+	bool flag = false;
+	if (m < 0){
+		m = 0 - m;
+		flag = true;
+	}
+	while (m){
+		s += table[m%n];
+		m /= n;
+	}
+	if (flag)
+		s += '-';
+	reverse(s.begin(), s.end());
+	cout << s << endl;
+	return 0;
+}
